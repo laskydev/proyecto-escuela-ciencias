a9f5b13ef67f892f837aa5c118e81433e161c75e
Eduardo Fortuna <eduardofortuna99@gmail.com>
2020-11-09 19:25:41 -0600

    Agregamos enlaces a los botones y colocamos animaciones

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,17 @@
+//Declaramos las secciones de la pagina
+const section1 = document.getElementById('s1')
+const section2 = document.getElementById('s2')
+const section3 = document.getElementById('s3')
+const section4 = document.getElementById('s4')
+
+//Declaramos los botones que hay en cada seccion
+const btn1 = document.getElementById('btn1')
+const btn2 = document.getElementById('btn2')
+const btn3 = document.getElementById('btn3')
+const btn4 = document.getElementById('btn4')
+
+
+
 //Funcion para dar el efecto parallax permanente
 function scale(section){
     section.animate([
@@ -11,20 +25,23 @@ function scale(section){
 //Funcion para que se mueva el botón de flecha que señala abajo
 function buttonTranslate(button){
     button.animate([
-        {transform: 'scale(1)'},
-        {transform: 'scale(1.010)'}],
+        {transform: 'translateY(0)'},
+        {transform: 'translateY(1rem)'}],
         {duration:1000, 
         direction:'alternate',
         iterations:Infinity})
 }
-const section1 = document.getElementById('s1')
-const section2 = document.getElementById('s2')
-const section3 = document.getElementById('s3')
-const section4 = document.getElementById('s4')
 
+
+//Inicializamos las funciones del parallax
 scale(section1)
 scale(section2)
 scale(section3)
 scale(section4)
+
+buttonTranslate(btn1)
+buttonTranslate(btn2)
+buttonTranslate(btn3)
+buttonTranslate(btn4)
 
 
